73391c7ddd8b91138ac39cd6f8322a5f52f61541
litehed <57595571+litehed@users.noreply.github.com>
2021-01-14 21:22:05 -0500

    Added Grabber and Wobble Goal commands and subsystem
    PogU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMain.java
@@ -13,33 +13,41 @@ import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.commands.Com_Grabber;
 import org.firstinspires.ftc.teamcode.commands.Com_Intake;
 import org.firstinspires.ftc.teamcode.commands.Com_Outtake;
+import org.firstinspires.ftc.teamcode.commands.Com_PickUp;
+import org.firstinspires.ftc.teamcode.commands.Com_PutDown;
 import org.firstinspires.ftc.teamcode.commands.Com_Shooter;
 import org.firstinspires.ftc.teamcode.commands.groups.SequentialShooter;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSystem;
 import org.firstinspires.ftc.teamcode.commands.drive.Com_Drive;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.WobbleSubsystem;
 import org.firstinspires.ftc.teamcode.util.TimedAction;
 
 @TeleOp(name="KekW")
 public class TeleMain extends CommandOpMode {
     //Servos and Motors
     private Motor fL, fR, bL, bR;
-    private Motor flyWheel, intakeA;
-    private SimpleServo flicker;
+    private Motor flyWheel, intakeA, arm;
+    private SimpleServo flicker, grabber;
 
     //Subsystems
     private DriveSystem driveSystem;
     private ShooterSubsystem shooterSystem;
     private IntakeSubsystem intakeSystem;
+    private WobbleSubsystem wobbleSystem;
 
     //Commands
     private Com_Drive driveCommand;
     private Com_Shooter shooterCommand;
     private Com_Intake intakeCommand;
     private Com_Outtake outtakeCommand;
+    private Com_Grabber grabberCommand;
+    private Com_PickUp pickUpCommand;
+    private Com_PutDown putDownCommand;
     private SequentialShooter shootCommandGroup;
     private InstantCommand runFlyWheelCommand;
 
@@ -62,8 +70,10 @@ public class TeleMain extends CommandOpMode {
         flyWheel = new Motor(hardwareMap, "shoot");
         flyWheel.setInverted(true);
         intakeA = new Motor(hardwareMap, "intakeA");
+        arm = new Motor(hardwareMap, "wobble", Motor.GoBILDA.RPM_312);
 
         flicker = new SimpleServo(hardwareMap, "flicker", 0, 270);
+        grabber = new SimpleServo(hardwareMap, "wobbleS", 0, 270);
 
         //imu
         imu = new RevIMU(hardwareMap);
@@ -83,7 +93,8 @@ public class TeleMain extends CommandOpMode {
 
         //Subsystems and Commands
         driveSystem = new DriveSystem(fL, fR, bL, bR);
-        driveCommand = new Com_Drive(driveSystem, m_driverOp::getLeftX, m_driverOp::getLeftY, m_driverOp::getRightX, ()->mult);
+        driveCommand = new Com_Drive(driveSystem, m_driverOp::getLeftX, m_driverOp::getLeftY,
+                m_driverOp::getRightX, ()->mult);
 
         shooterSystem = new ShooterSubsystem(flyWheel, flicker, flickerAction, telemetry);
         shooterCommand = new Com_Shooter(shooterSystem);
@@ -94,6 +105,11 @@ public class TeleMain extends CommandOpMode {
         intakeSystem = new IntakeSubsystem(intakeA);
         intakeCommand = new Com_Intake(intakeSystem);
         outtakeCommand = new Com_Outtake(intakeSystem);
+
+        wobbleSystem = new WobbleSubsystem(arm, grabber);
+        grabberCommand = new Com_Grabber(wobbleSystem);
+        pickUpCommand = new Com_PickUp(wobbleSystem);
+        putDownCommand = new Com_PutDown(wobbleSystem);
 
 //       Old Method no longer necessary:
 //        slowDrive = new GamepadButton(m_driverOp, GamepadKeys.Button.Y)
@@ -106,6 +122,9 @@ public class TeleMain extends CommandOpMode {
 
         m_driverOp.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenHeld(intakeCommand);
         m_driverOp.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenHeld(outtakeCommand);
+
+        m_driverOp.getGamepadButton(GamepadKeys.Button.X).whenPressed(grabberCommand);
+        m_driverOp.getGamepadButton(GamepadKeys.Button.B).toggleWhenPressed(pickUpCommand, putDownCommand);
 
         register(driveSystem);
         driveSystem.setDefaultCommand(driveCommand);
