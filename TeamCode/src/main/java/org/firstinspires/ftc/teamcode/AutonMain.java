1e55c9ec4ea14a16c81a1221cd0e95df949030bb
JIceberg <jisenberg3@gatech.edu>
2021-02-24 23:20:41 -0500

    I swear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMain.java
@@ -62,7 +62,7 @@ public class AutonMain extends CommandOpMode {
         intakeB = new Motor(hardwareMap, "intakeB", Motor.GoBILDA.RPM_312);
         intakeB.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         intakeB.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        intakeA = new Motor(hardwareMap, "intakeA", Motor.GoBILDA.RPM_312);
+        intakeA = new Motor(hardwareMap, "intakeA", Motor.GoBILDA.RPM_1150);
 
 
         arm.motor.setDirection(DcMotor.Direction.REVERSE);
@@ -84,7 +84,7 @@ public class AutonMain extends CommandOpMode {
 
         shooterSystem = new ShooterSubsystem(flyWheel, flicker, flickerAction, voltageSensor);
 
-//        intakeSystem = new IntakeSubsystem(intakeA, intakeB);
+        intakeSystem = new IntakeSubsystem(intakeA, intakeB);
 
         ugContourRingDetector = new UGContourRingDetector(hardwareMap, "poopcam", telemetry, true);
         ugContourRingDetector.init();
@@ -100,7 +100,7 @@ public class AutonMain extends CommandOpMode {
 //                new WaitUntilCommand(this::isStarted),  Jacksons favorite line of code
                 new SelectCommand(new HashMap<Object, Command>() {{
                         put(VisionSystem.Size.ZERO, (new ZeroRing(drive, wobble, shooterSystem)));
-                        put(VisionSystem.Size.ONE, (new OneRing(drive, wobble, shooterSystem)));
+                        put(VisionSystem.Size.ONE, (new OneRing(drive, wobble, shooterSystem, intakeSystem)));
                         put(VisionSystem.Size.FOUR, (new FourRing(drive, wobble, shooterSystem)));
                     }},()-> height)
         );
